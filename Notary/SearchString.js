20ddf29f1536041179750024a0e7327f8796d947
Calvin Moore <95387589+calvinM17@users.noreply.github.com>
2021-12-27 14:57:12 -0600

    Add files via upload

---
+++ Notary/SearchString.js
@@ -0,0 +1,32 @@
+//Load a book from disk
+function loadBook(filename, displayName){
+    let currentBook = "";
+    let url = "books/" + filename;
+
+    //reset our UI
+    document.getElementById("fileName").innerHTML = displayName;
+    document.getElementById("searchstat").innerHTML = "";
+    document.getElementById("keyword").Value = "";
+
+    //create a server a request to load our book
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            currentBook = xhr.responseText;
+
+             //remove line breaks and carriage returns and replace with a <br>
+             currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
+            document.getElementById("fileContent").innerHTML = currentBook;
+
+           
+
+
+            var elmnt = document.getElementById("fileContent");
+            elmnt.scrollTop = 0;
+        }
+    };
+}
